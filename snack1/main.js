310370a2262ff05e3e46192aac8f9d4630cf4936
alessiaBer <alexdeyn@gmail.com>
2023-03-24 12:16:38 +0100

    initial commit

---
+++ snack1/main.js
@@ -0,0 +1,62 @@
+/* Crea un array di 10 oggetti che rappresentano una zucchina, 
+indicando per ognuna varietà, peso e lunghezza. 
+Calcola quanto pesano tutte le zucchine. */
+
+
+// creo un array di 10 oggetti
+const courgette = [
+    {
+        variety: 'Nero di Milano',
+        weight: 100,
+        length: 14
+    },
+    {
+        variety: 'Romanesco',
+        weight: 70,
+        length: 10
+    },
+    {
+        variety: 'Ortolano di Faenza',
+        weight: 120,
+        length: 7
+    },
+    {
+        variety: 'Lunga fiorentina',
+        weight: 150,
+        length: 20
+    },
+    {
+        variety: 'Siciliano',
+        weight: 225,
+        length: 50
+    },
+    {
+        variety: 'Striata di Napoli',
+        weight: 110,
+        length: 16
+    },
+    {
+        variety: 'Bianca triestina',
+        weight: 90,
+        length: 10
+    },
+    {
+        variety: 'Tondo di Piacenza',
+        weight: 180,
+        length: 8
+    },
+    {
+        variety: 'Tondo di Nizza',
+        weight: 180,
+        length: 7
+    },
+    {
+        variety: 'Trombetta di Albenga',
+        weight: 500,
+        length: 60
+    }
+]
+
+//con il forEach estrapolo il peso di ciascun oggetto
+
+//con return restituisco la somma
