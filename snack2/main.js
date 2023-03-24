75deed63462907e1861ae652c214e430a0d2e9d6
alessiaBer <alexdeyn@gmail.com>
2023-03-24 12:42:38 +0100

    initial commit

---
+++ snack2/main.js
@@ -0,0 +1,13 @@
+/* Scrivi una funzione che accetti tre argomenti:
+un array e due numeri ("a" più piccolo di "b" e "b" grande al massimo quanto il numero di elementi dell'array).
+La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra "a" e "b" */
+
+//dichiaro una funzione che accetti 3 param
+
+//definisco le condizioni di a 
+
+//definisco le condizioni di b 
+
+//con filter() verifico la condizione di esistenza dei valori comprsi tra a e b
+
+//ritorno i valori compresi tra a e b
